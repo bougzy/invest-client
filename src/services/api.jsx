03bb8fcec48fd727ad76d48cbed9a3c56ec7a3f8
bougzy <yubeespecial@gmail.com>
2024-09-03 19:39:50 +0100

    update

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -4,7 +4,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://invest-api-lilac.vercel.app/api';
 
 const API = axios.create({
   baseURL: API_BASE_URL,
